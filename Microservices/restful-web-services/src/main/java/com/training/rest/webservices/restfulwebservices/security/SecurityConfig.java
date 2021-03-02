40163fcfb09c12521f1be9ced0081e16bb2edf2a
hlibman-kc <henry.libman@kinandcarta.com>
2021-03-02 13:02:57 -0600

    Finished Restful Web Services

---
+++ Microservices/restful-web-services/src/main/java/com/training/rest/webservices/restfulwebservices/security/SecurityConfig.java
@@ -0,0 +1,37 @@
+/* package com.training.rest.webservices.restfulwebservices.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    UserConfig config;
+
+    public SecurityConfig(UserConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        http
+                .csrf().disable()
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception
+    {
+        auth.inMemoryAuthentication()
+                .withUser(config.getUserName())
+                .password("{noop}"+config.getPassword())
+                .roles("USER");
+    }
+}*/
---
+++ Microservices/restful-web-services/src/main/java/com/training/rest/webservices/restfulwebservices/security/UserConfig.java
@@ -0,0 +1,23 @@
+/*package com.training.rest.webservices.restfulwebservices.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+@Configuration
+@PropertySource("classpath:application.properties")
+public class UserConfig {
+
+    @Value("${spring.security.user.name}")
+    private String userName;
+
+    @Value("${spring.security.user.password}")
+    private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}*/
